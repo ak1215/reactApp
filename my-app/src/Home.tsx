92e857be9bd9a01bb1f7b25ad69dc76b08c808e2
ak1215 <60421957+ak1215@users.noreply.github.com>
2021-09-23 09:57:19 +0200

    Form added

--- my-app/src/Home.tsx
+++ my-app/src/Home.tsx
@@ -1,48 +1,55 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Carousel } from "react-bootstrap";
+import { Carousel, FormControl, InputGroup } from "react-bootstrap";
 import "./css/Home.css";
 
 export default function Home() {
   return (
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://i.pinimg.com/736x/c8/7b/62/c87b625a9dac4b63c19ec84ff7f276ab.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://cdn.vox-cdn.com/thumbor/Zwx4YKd471CKB2nOyTf3PyBLItQ=/0x0:6000x4000/1200x800/filters:focal(1978x1291:2938x2251)/cdn.vox-cdn.com/uploads/chorus_image/image/65776271/shutterstock_557338165.0.jpg"
-          alt="Second slide"
-        />
+    <>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://i.pinimg.com/736x/c8/7b/62/c87b625a9dac4b63c19ec84ff7f276ab.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://cdn.vox-cdn.com/thumbor/Zwx4YKd471CKB2nOyTf3PyBLItQ=/0x0:6000x4000/1200x800/filters:focal(1978x1291:2938x2251)/cdn.vox-cdn.com/uploads/chorus_image/image/65776271/shutterstock_557338165.0.jpg"
+            alt="Second slide"
+          />
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://img.jamieoliver.com/jamieoliver/recipe-database/oldImages/large/1235_1_1436889055.jpg?tr=w-800,h-1066"
-          alt="Third slide"
-        />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://img.jamieoliver.com/jamieoliver/recipe-database/oldImages/large/1235_1_1436889055.jpg?tr=w-800,h-1066"
+            alt="Third slide"
+          />
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      <InputGroup className="mb-3">
+        <InputGroup.Text>First and last name</InputGroup.Text>
+        <FormControl aria-label="First name" />
+        <FormControl aria-label="Last name" />
+      </InputGroup>
+    </>
   );
 }
